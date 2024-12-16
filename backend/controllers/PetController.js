b3c5a1c095afe54f1789ddea275184a9c2616104
IsabelleHeloisa <isabelle.hello123@outlook.com>
2024-12-16 14:18:52 -0300

    feat: removing pet from the system

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -107,7 +107,7 @@ module.exports = class PetController {
 
   static async getPetById(req, res) {
     const id = req.params.id
-
+    // check if id id valid
     if (!ObjectId.isValid(id)) {
       res.status(422).json({ message: 'ID inválido' })
       return
@@ -123,5 +123,38 @@ module.exports = class PetController {
     res.status(200).json({
       pet: pet
     })
+  }
+
+  static async removePetById(req, res) {
+    const id = req.params.id
+    // check if id id valid
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({ message: 'ID inválido' })
+      return
+    }
+    // check if pet exists
+    const pet = await Pet.findOne({ _id: id })
+
+    if (!pet) {
+      res.status(404).json({ message: 'Pet não encontrado' })
+      return
+    }
+
+    //check if logged in user registered the pet
+
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    if (pet.user._id.toString() !== user._id.toString()) {
+      res.status(404).json({
+        message:
+          'Houve um problema em processar a sua solicitação, tente novamente mais tarde!'
+      })
+      return
+    }
+
+    await Pet.findByIdAndDelete(id)
+
+    res.status(200).json({ message: 'Pet removido com sucesso!' })
   }
 }
