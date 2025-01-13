9ff1f1ff15c272ea8f0dc410bda5a9ccc75e253c
IsabelleHeloisa <isabelle.hello123@outlook.com>
2025-01-13 15:17:16 -0300

    feat: structuring navbar and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+// components
+import Navbar from './components/layout/Navbar.jsx'
+import Footer from './components/layout/Footer.jsx'
+
 // pages
-import Login from './components/pages/Auth/Login.js'
-import Register from './components/pages/Auth/Register.js'
-import Home from './components/pages/Home.js'
+import Login from './components/pages/Auth/Login.jsx'
+import Register from './components/pages/Auth/Register.jsx'
+import Home from './components/pages/Home.jsx'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <span className="bold">Get a Pet</span> &copy; 2025
+      </p>
+    </footer>
+  )
+}
+export default Footer
