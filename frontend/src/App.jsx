ad39c3e1a0a6196d6effa84495a3c99572b9cd1f
IsabelleHeloisa <isabelle.hello123@outlook.com>
2025-01-17 13:27:55 -0300

    feat: create user context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,18 +10,22 @@ import Register from './components/pages/Auth/Register.jsx'
 import Home from './components/pages/Home.jsx'
 import Container from './components/layout/Container'
 
+import { UserProvider } from './context/UserContext.jsx'
+
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Container>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </UserProvider>
     </Router>
   )
 }
