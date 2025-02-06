9a4ece2c7a61845481ddf28c84256325803c248f
IsabelleHeloisa <isabelle.hello123@outlook.com>
2025-02-06 15:49:40 -0300

    feat: displaying pet on dashboard

--- frontend/src/components/pages/Pet/MyPets.jsx
+++ frontend/src/components/pages/Pet/MyPets.jsx
@@ -1,4 +1,5 @@
 import api from '../../../utils/api'
+import styles from './Dashboard.module.css'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import RoudedImage from '../../layout/RoudedImage'
@@ -25,7 +26,28 @@ function MyPets() {
         <Link to="/pet/add">Cadastrar Pet</Link>
       </div>
       <div>
-        {pets.length > 0 && <p>Meus pets cadastrados</p>}
+        {pets.length > 0 &&
+          pets.map(pet => (
+            <div key={pet._id}>
+              <RoudedImage
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="75px"
+              />
+              <span className="bold">{pet.name}</span>
+              <div className={StyleSheet.action}>
+                {pet.available ? (
+                  <>
+                    {pet.adopter && <button>Concluir adoção</button>}
+                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                    <button>Excluir</button>
+                  </>
+                ) : (
+                  <p>Pet já adotado</p>
+                )}
+              </div>
+            </div>
+          ))}
         {pets.length === 0 && <p>Não há pets cadstrados</p>}
       </div>
     </section>
