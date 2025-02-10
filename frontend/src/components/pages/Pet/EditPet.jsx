81665b884ce567ddeb65b87c3a3a76957729aeb1
IsabelleHeloisa <isabelle.hello123@outlook.com>
2025-02-10 11:32:45 -0300

    feat: filling out the edit pet data form

--- frontend/src/components/pages/Pet/EditPet.jsx
+++ frontend/src/components/pages/Pet/EditPet.jsx
@@ -6,15 +6,35 @@ import styles from './AddPet.module.css'
 
 import PetForm from '../../form/PetForm'
 
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function EditPet() {
+  const [pet, setPet] = useState({})
+  const [token] = useState(localStorage.getItem('token') || '')
+  const { id } = useParams()
+  const Navigate = useNavigate()
+
+  useEffect(() => {
+    api
+      .get(`/pets/${id}`, {
+        Authorization: `Bearer ${token}`
+      })
+      .then(response => {
+        setPet(response.data.pet)
+      })
+  }, [token, id])
+
+  async function updatePet(pet) {}
+
   return (
     <section>
       <div className={styles.addpet_header}>
-        <h1>Editando o Pet: 'pet.name'</h1>
+        <h1>Editando o Pet: {pet.name}</h1>
         <p>Depois da edição os dados serão atualizados no sistema</p>
       </div>
+      {pet.name && (
+        <PetForm handleSubmit={updatePet} btnText="Atualizar" petData={pet} />
+      )}
     </section>
   )
 }
