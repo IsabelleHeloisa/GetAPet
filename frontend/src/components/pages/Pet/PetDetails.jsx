9b02f188b962158fb49cf9232a6724c37f001dfd
IsabelleHeloisa <isabelle.hello123@outlook.com>
2025-02-10 14:06:41 -0300

    feat: searching for pet via api

--- frontend/src/components/pages/Pet/PetDetails.jsx
+++ frontend/src/components/pages/Pet/PetDetails.jsx
@@ -2,13 +2,24 @@ import api from '../../../utils/api'
 
 import { useState, useEffect } from 'react'
 
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 import styles from './PetDetails.module.css'
 
 import { toast } from 'react-toastify'
 
 function PetDatails() {
-  return <h1>Página de pet</h1>
+  const [pet, setPet] = useState({})
+  const { id } = useParams()
+  const Navigate = useNavigate()
+  const [token] = useState(localStorage.getItem('token') || '')
+
+  useEffect(() => {
+    api.get(`/pets/${id}`).then(response => {
+      setPet(response.data.pet)
+    })
+  }, [id])
+
+  return <h1>{pet.name}</h1>
 }
 
 export default PetDatails
